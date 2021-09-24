19779dc7c8db52d80409073b24bb16549550dbce
JoelynZhou <joelynzhou@hotmail.com>
2021-09-24 11:05:39 +0800

    chore: HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	entry: {
 		app: "./src/index.js",
 		print: "./src/print.js",
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "管理输出",
+		}),
+	],
 	output: {
 		// !除此以外，还需要去 index.html 里面修改旧的名称，如何解决？用 HtmlWebpackPlugin 插件
 		filename: "[name].bundle.js", //根据入口起点定义的名称，动态产生 bundle 名称
