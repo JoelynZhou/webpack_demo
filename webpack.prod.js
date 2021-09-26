9d228ef3a5967c6f9856e26a0f6c1ae6859a3815
JoelynZhou <joelynzhou@hotmail.com>
2021-09-26 13:41:23 +0800

    chore: source map

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,4 +3,5 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
 	mode: "production",
+	devtool: "source-map", //鼓励在生产环境使用 source-map，他们对 debug 和基准测试有帮助，但避免使用 inline-source-map，会增加 bundle 体积大小，降低整体性能
 });
